9a1920b855945209ba1140514283b7bf02214c6d
abhinav <abhinav.drs@gmail.com>
2020-08-03 16:26:29 +0200

    added multiprocessing

--- usage/videoToframes.py
+++ usage/videoToframes.py
@@ -1,12 +1,15 @@
 """
-videoToframes.py takes a video as input and stores extracted framesin Occlusion_Net/Demo folder. 
-the path to the videofile (including the name of video file itself) must be passed as -path .
+videoToframes.py takes a video as input and stores extracted frames in Occlusion_Net/demo folder.
+The path to the videofile (including the name of video file itself) must be passed as -path .
 """
-
+from concurrent.futures import ProcessPoolExecutor, as_completed
 import cv2
-import time
+import multiprocessing
 import os
+import sys
 import argparse
+
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description = 'Path to video')
@@ -20,51 +23,133 @@ def dir_path(path):
     else:
         raise argparse.ArgumentTypeError(f"Video_Path:{path} is not valid")
 
-def video_to_frames(input_loc, output_loc):
-    """Function to extract frames from input video file
-    and save them as separate frames in an output directory.
-    Args:
-        input_loc: Input video file.
-        output_loc: Output directory to save the frames.
-    Returns:
-        None
+
+def print_progress(iteration, total, prefix='', suffix='', decimals=3, bar_length=100):
     """
-    try:
-        os.mkdir(output_loc)
-    except OSError:
-        pass
-    # Log the time
-    time_start = time.time()
-    # Start capturing the feed
-    cap = cv2.VideoCapture(input_loc)
-    # Find the number of frames
-    video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
-    print ("Number of frames: ", video_length)
-    count = 0
-    print ("Converting video..\n")
-    # Start converting the video
-    while cap.isOpened():
-        # Extract the frame
-        ret, frame = cap.read()
-        # Write the results back to output location.
-        cv2.imwrite(output_loc + "/%#05d.jpg" % (count+1), frame)
-        count = count + 1
-        # If there are no more frames left
-        if (count > (video_length-1)):
-            # Log the time again
-            time_end = time.time()
-            # Release the feed
-            cap.release()
-            # Print stats
-            print ("Done extracting frames.\n%d frames extracted" % count)
-            print ("It took %d seconds forconversion." % (time_end-time_start))
+    Call in a loop to create standard out progress bar
+    :param iteration: current iteration
+    :param total: total iterations
+    :param prefix: prefix string
+    :param suffix: suffix string
+    :param decimals: positive number of decimals in percent complete
+    :param bar_length: character length of bar
+    :return: None
+    """
+
+    format_str = "{0:." + str(decimals) + "f}"  # format the % done number string
+    percents = format_str.format(100 * (iteration / float(total)))  # calculate the % done
+    filled_length = int(round(bar_length * iteration / float(total)))  # calculate the filled bar length
+    bar = '#' * filled_length + '-' * (bar_length - filled_length)  # generate the bar string
+    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix)),  # write out the bar
+    sys.stdout.flush()  # flush to stdout
+
+
+def extract_frames(video_path, frames_dir, overwrite=False, start=-1, end=-1, every=1):
+    """
+    Extract frames from a video using OpenCVs VideoCapture
+    :param video_path: path of the video
+    :param frames_dir: the directory to save the frames
+    :param overwrite: to overwrite frames that already exist?
+    :param start: start frame
+    :param end: end frame
+    :param every: frame spacing
+    :return: count of images saved
+    """
+
+    video_path = os.path.normpath(video_path)  # make the paths OS (Windows) compatible
+    frames_dir = os.path.normpath(frames_dir)  # make the paths OS (Windows) compatible
+
+    video_dir, video_filename = os.path.split(video_path)  # get the video path and filename from the path
+
+    assert os.path.exists(video_path)  # assert the video file exists
+
+    capture = cv2.VideoCapture(video_path)  # open the video using OpenCV
+
+    if start < 0:  # if start isn't specified lets assume 0
+        start = 0
+    if end < 0:  # if end isn't specified assume the end of the video
+        end = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    capture.set(1, start)  # set the starting frame of the capture
+    frame = start  # keep track of which frame we are up to, starting from start
+    while_safety = 0  # a safety counter to ensure we don't enter an infinite while loop (hopefully we won't need it)
+    saved_count = 0  # a count of how many frames we have saved
+
+    while frame < end:  # lets loop through the frames until the end
+
+        _, image = capture.read()  # read an image from the capture
+
+        if while_safety > 500:  # break the while if our safety maxs out at 500
             break
+
+        # sometimes OpenCV reads None's during a video, in which case we want to just skip
+        if image is None:  # if we get a bad return flag or the image we read is None, lets not save
+            while_safety += 1  # add 1 to our while safety, since we skip before incrementing our frame variable
+            continue  # skip
+
+        if frame % every == 0:  # if this is a frame we want to write out based on the 'every' argument
+            while_safety = 0  # reset the safety count
+            save_path = os.path.join(frames_dir, "{:010d}.jpg".format(frame))  # create the save path
+            if not os.path.exists(save_path) or overwrite:  # if it doesn't exist or we want to overwrite anyways
+                cv2.imwrite(save_path, image)  # save the extracted image
+                saved_count += 1  # increment our counter by one
+
+        frame += 1  # increment our frame count
+
+    capture.release()  # after the while has finished close the capture
+
+    return saved_count  # and return the count of the images we saved
+
+
+def video_to_frames(video_path, frames_dir, overwrite=False, every=1, chunk_size=1000):
+    """
+    Extracts the frames from a video using multiprocessing
+    :param video_path: path to the video
+    :param frames_dir: directory to save the frames
+    :param overwrite: overwrite frames if they exist?
+    :param every: extract every this many frames
+    :param chunk_size: how many frames to split into chunks (one chunk per cpu core process)
+    :return: path to the directory where the frames were saved, or None if fails
+    """
+
+    video_path = os.path.normpath(video_path)  # make the paths OS (Windows) compatible
+    frames_dir = os.path.normpath(frames_dir)  # make the paths OS (Windows) compatible
+
+    video_dir, video_filename = os.path.split(video_path)  # get the video path and filename from the path
+
+    # make directory to save frames, its a sub dir in the frames_dir with the video name
+    #os.makedirs(os.path.join(frames_dir, video_filename), exist_ok=True)
+
+    capture = cv2.VideoCapture(video_path)  # load the video
+    total = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))  # get its total frame count
+    capture.release()  # release the capture straight away
+    print('Number of frames: ', total)
+
+    if total < 1:  # if video has no frames, might be and opencv error
+        print("Video has no frames. Check your OpenCV + ffmpeg installation")
+        return None  # return None
+
+    frame_chunks = [[i, i+chunk_size] for i in range(0, total, chunk_size)]  # split the frames into chunk lists
+    frame_chunks[-1][-1] = min(frame_chunks[-1][-1], total-1)  # make sure last chunk has correct end frame, also handles case chunk_size < total
+
+    prefix_str = "Extracting frames from {}".format(video_filename)  # a prefix string to be printed in progress bar
+
+    # execute across multiple cpu cores to speed up processing, get the count automatically
+    with ProcessPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
+
+        futures = [executor.submit(extract_frames, video_path, frames_dir, overwrite, f[0], f[1], every)
+                   for f in frame_chunks]  # submit the processes: extract_frames(...)
+
+        for i, f in enumerate(as_completed(futures)):  # as each process completes
+            print_progress(i, len(frame_chunks)-1, prefix=prefix_str, suffix='Complete')  # print it's progress
+
+    return os.path.join(frames_dir, video_filename)  # when done return the directory containing the frames
+
 
 def main():
     parsed_args = parse_arguments()
-    input_loc = parsed_args.path
-    output_loc = os.path.dirname(os.getcwd()) + '/demo'
-    video_to_frames(input_loc, output_loc)
+    path_to_video = parsed_args.path
+    path_to_frames = os.path.dirname(os.getcwd()) + '/demo'
+    video_to_frames(video_path=path_to_video, frames_dir=path_to_frames, overwrite=False, every=1, chunk_size=1000)
 
 
 if __name__=="__main__":
