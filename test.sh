7b40d4563a004ca34c54082c363091b63b597682
abhinav <abhinav.drs@gmail.com>
2020-08-19 16:27:06 +0200

    Updated to accept videofile as an argument

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-docker run -v $PWD:/code --shm-size=32GB --runtime=nvidia $1 python infer.py -ir $2 -v True -cfg data/occlusion_net_test.yaml
+docker run -v $PWD:/code --shm-size=32GB --runtime=nvidia $1 python infer.py -vid $2 -v True -cfg data/occlusion_net_test.yaml
